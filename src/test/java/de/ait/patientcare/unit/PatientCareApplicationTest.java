6b4f23c64775955fe6ec45f3b572b98e00a1e94a
Alexander Hermann <ewebotah@gmail.com>
2026-01-30 01:45:14 +0100

    test(unit): add PatientCareApplicationTest for application context loading

---
+++ src/test/java/de/ait/patientcare/unit/PatientCareApplicationTest.java
@@ -0,0 +1,22 @@
+package de.ait.patientcare.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Alexander Hermann
+ * Created : 30.01.2026
+ * Project : PatientCare
+ * ----------------------------------------------------------------------------
+ */
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PatientCareApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
